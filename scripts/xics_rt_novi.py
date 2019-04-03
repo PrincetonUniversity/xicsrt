Fixed an issue in the voigt cdf scaling. Updated the Voigt and Normal wavelength generators.

scripts/xics_rt_novi.py
@@ -27,7 +27,6 @@ from xicsrt.xics_rt_optics import SphericalCrystal
 from xicsrt.xics_rt_raytrace import raytrace, raytrace_special
 from xicsrt.xics_rt_tools import source_location_bragg
 
-float64 = np.float64
 profiler.stop('Import Time')
 
 
@@ -45,7 +44,7 @@ input = OrderedDict()
 # A source intensity greater than 1e7 is not recommended due to excessive
 # memory usage.
 input['source_intensity']= int(1e7)
-input['number_of_runs'] = 10
+input['number_of_runs'] = 100
 
 
 # Evetually I should just read all of these numbers directly
@@ -99,13 +98,10 @@ input['source_orientation'] /= np.linalg.norm(input['source_orientation'])
 # This needs to be matched to the source distance and crystal size.
 # At the moment this is too small to fully illuminate the crystal.
 input['source_spread']   = 1.0
-
 # Ion temperature in eV
 input['source_temp']     = 1800
-
 # Argon mass in AMU.
 input['source_mass']     = 39.948
-
 # Naturaly linewith for the Ar16+ w line. Units: 1/s.
 input['natural_linewidth'] = 1.129e+14
 
@@ -113,7 +109,7 @@ input['natural_linewidth'] = 1.129e+14
 # These values are arbitrary for now.
 input['source_width']   = 0.15
 input['source_height']  = 0.75
-input['source_depth']   = 0.0
+input['source_depth']   = 1.0
 
 
 

xicsrt/math/voigt.py
@@ -59,8 +59,8 @@ def voigt_cdf_tab(gamma, sigma, gridsize=None, cutoff=None):
     gridsize_min = 100
 
     percent = 0.5
-    gauss_hwpm = np.sqrt(2.0*np.log(1.0/percent))*param['sigma']
-    lorentz_hwpm = param['gamma']*np.sqrt(1.0/percent - 1.0)
+    gauss_hwpm = np.sqrt(2.0*np.log(1.0/percent))*sigma
+    lorentz_hwpm = gamma*np.sqrt(1.0/percent - 1.0)
     # This is always larger than the voigt hwpm (half width at percent max).
     hwpm_max = np.sqrt(gauss_hwpm**2 + lorentz_hwpm**2)
 
@@ -68,11 +68,11 @@ def voigt_cdf_tab(gamma, sigma, gridsize=None, cutoff=None):
     value = gridsize_min/2*min_spacing
 
     # Far from the peak, a Voigt profile is always less than a Lorentzian.
-    lorentz_cutoff = param['gamma']*np.sqrt(1.0/cutoff - 1.0)
-    base = np.exp(1/value * np.log(lorentz_cutoff/value))
+    lorentz_cutoff = gamma*np.sqrt(1.0/cutoff - 1.0)
+    base = np.exp(1/10 * np.log(lorentz_cutoff/value))
 
     bounds = np.linspace(-value, value, gridsize+1)
-    bounds = bounds*base**np.abs(bounds)
+    bounds = bounds*base**np.abs(bounds/value*10)
     cdf_x = (bounds[:-1]+bounds[1:])/2
         
     # We must used a properly normalized voigt here (intensity=1.0)

xicsrt/xics_rt_sources.py
@@ -6,7 +6,7 @@ Created on Mon Nov 13 10:12:15 2017
 """
 import numpy as np   
 from scipy.stats import cauchy        
-import scipy.constants as constants
+import scipy.constants as const
 
 from xicsrt.util import profiler
 from xicsrt.math import voigt
@@ -495,43 +495,72 @@ class ExtendedSource:
           temperature: eV
         """
 
+        # Check for the trivial case.
+        if (self.natural_linewidth  == 0.0 and self.temp == 0.0) :
+            return np.ones(size)*self.wavelength
+        
+        # Check for the Gaussian case.
+        if (self.temp == 0.0):
+            return random_wavelength_normal(size)
+        
+        # Check for the lorentizan case.
+        if (self.natural_linewidth  == 0.0):
+            # I need to update the cauchy routine first.
+            pass
+        
         c = const.physical_constants['speed of light in vacuum'][0]
         amu_kg = const.physical_constants['atomic mass unit-kilogram relationship'][0]
-        ev_j = const.physical_constants['electron volt-joule relationship'][0]
+        ev_J = const.physical_constants['electron volt-joule relationship'][0]
         
         # Natural line width.
         gamma = ( self.natural_linewidth * self.wavelength**2
                   / (4 * np.pi * c * 1e10) )
 
         # Doppler broadened line width.
-        sigma = ( np.sqrt(self.temp / self.mass_number / amu_kg / c**2 * ev_J * 1e3)
+        sigma = ( np.sqrt(self.temp / self.mass_number / amu_kg / c**2 * ev_J)
                   * self.wavelength )
 
-        rand_wave = voigt_random(gamma, sigma, size)
+        print('sigma:', sigma, 'gamma:', gamma)
+
+        rand_wave = voigt.voigt_random(gamma, sigma, size)
         rand_wave += self.wavelength
         
         return rand_wave
 
     
     def random_wavelength_normal(self, size=None):
+        """
+        Units:
+          wavelength: angstroms
+          temperature: eV
+        """
         
-        c = 3.00e18                         # angstroms per sec
-        conv = 931.4940954e6                # eV per atomic u
-        mc2 = self.mass_number * conv       # mass energy in eV     
-                
-        mean_wave = self.wavelength
-        mean_freq =  c / mean_wave
+        c = const.physical_constants['speed of light in vacuum'][0]
+        amu_kg = const.physical_constants['atomic mass unit-kilogram relationship'][0]
+        ev_J = const.physical_constants['electron volt-joule relationship'][0]
         
-        sigma = np.sqrt(self.temp / mc2) * mean_freq
-        rand_freq = np.random.normal(mean_freq, sigma, size)
-        rand_wave = c / rand_freq
+        # Doppler broadened line width.
+        sigma = ( np.sqrt(self.temp / self.mass_number / amu_kg / c**2 * ev_J)
+                  * self.wavelength )
+
+        rand_wave = np.random.normal(self.wavelength, sigma, size)
         
         return rand_wave
 
     
     def random_wavelength_cauchy(self, size=None):
+
+        # This function needs to be updated to use the same definitions
+        # as random_wavelength_voigt.
+        #
+        # As currently writen natual_linewidth is not used in a way
+        # consistent with physical units.
+        #
+        # It also may make sense to add some sort of cutoff here.
+        # the extreme tails of the distribution are not really useful
+        # for ray tracing.
         fwhm = self.natural_linewidth
-        rand_wave = cauchy.rvs(loc = self.wavelength, scale = fwhm, size = size)
+        rand_wave = cauchy.rvs(loc=self.wavelength, scale=fwhm, size=size)
     
         return rand_wave
 
@@ -576,7 +605,7 @@ class ExtendedSource:
     def generate_wavelength(self):
         #random_wavelength = self.random_wavelength_normal
         #random_wavelength = self.random_wavelength_cauchy
-        random_wavelength = self.random_wavelength_normal
+        random_wavelength = self.random_wavelength_voigt
         
         wavelength = random_wavelength(self.intensity)
         wavelength = wavelength[:, np.newaxis]