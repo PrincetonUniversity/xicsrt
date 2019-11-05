f6175a6137248eb8d77f9caa7eeec50bee1fad09
Novimir Antoniuk Pablant <novimir.pablant@amicitas.com>
2019-11-05 16:31:21 -0500

    Separated raytrace and raytrace_single. raytrace now separates histories for found and lost rays and truncates the number of lost rays.  This helps with visualization.  My configuration generation script now uses a function. other small changes.

--- scripts/xicsrt_iterstudy_npablant.py
+++ scripts/xicsrt_iterstudy_npablant.py
@@ -17,191 +17,194 @@ This script creates a default configuration for a single XICS run of the ITER XR
 import numpy as np
 from collections import OrderedDict
 
-config = OrderedDict()
-config['general_input']   = OrderedDict()
-config['scenario_input']  = OrderedDict()
-config['plasma_input']    = OrderedDict()
-config['source_input']    = OrderedDict()
-config['graphite_input']  = OrderedDict()
-config['crystal_input']   = OrderedDict()
-config['detector_input']  = OrderedDict()
+def get_config():
 
-# -----------------------------------------------------------------------------
-# General raytracer properties
+    config = OrderedDict()
+    config['general_input']   = OrderedDict()
+    config['scenario_input']  = OrderedDict()
+    config['plasma_input']    = OrderedDict()
+    config['source_input']    = OrderedDict()
+    config['graphite_input']  = OrderedDict()
+    config['crystal_input']   = OrderedDict()
+    config['detector_input']  = OrderedDict()
 
-# set ideal_geometry to False to enable offsets, tilts, and thermal expand
-# set backwards_raytrace to True to swap the detector and source
-# set do_visualizations to toggle the visualizations on or off
-# set do_savefiles to toggle whether the program saves .tif files
-# set do_image_analysis to toggle whether the visualizer performs .tif analysis
-# set do_bragg_checks to False to make the optics into perfect X-Ray mirrors
-# set do_miss_checks to False to prevent optics from masking rays that miss
-# change the random seed to alter the random numbers generated
-# possible scenarios include 'MODEL', 'PLASMA', 'BEAM', 'CRYSTAL', 'GRAPHITE', 'SOURCE'
-# possible rocking curve types include 'STEP', 'GAUSS', and 'FILE'
+    # -----------------------------------------------------------------------------
+    # General raytracer properties
 
-config['general_input']['number_of_rays']     = int(1e6)
-config['general_input']['number_of_runs']     = 1
+    # set ideal_geometry to False to enable offsets, tilts, and thermal expand
+    # set backwards_raytrace to True to swap the detector and source
+    # set do_visualizations to toggle the visualizations on or off
+    # set do_savefiles to toggle whether the program saves .tif files
+    # set do_image_analysis to toggle whether the visualizer performs .tif analysis
+    # set do_bragg_checks to False to make the optics into perfect X-Ray mirrors
+    # set do_miss_checks to False to prevent optics from masking rays that miss
+    # change the random seed to alter the random numbers generated
+    # possible scenarios include 'MODEL', 'PLASMA', 'BEAM', 'CRYSTAL', 'GRAPHITE', 'SOURCE'
+    # possible rocking curve types include 'STEP', 'GAUSS', and 'FILE'
 
-config['general_input']['ouput_path']         = '/u/npablant/code/mirproject/xicsrt/results/'
-config['general_input']['ideal_geometry']     = True
-config['general_input']['backwards_raytrace'] = False
-config['general_input']['do_visualizations']  = False
-config['general_input']['do_savefiles']       = False
-config['general_input']['do_image_analysis']  = False
-config['general_input']['random_seed']        = 123456
-config['general_input']['scenario']           = 'BEAM'
-config['general_input']['system']             = 'w7x_ar16'
-config['general_input']['shot']               = 180707017
+    config['general_input']['number_of_rays']     = int(1e6)
+    config['general_input']['number_of_runs']     = 1
 
-# Temperature that the optical elements will be cooled to (kelvin)
-config['general_input']['xics_temp'] = 273.0
+    config['general_input']['ouput_path']         = '/u/npablant/code/mirproject/xicsrt/results/'
+    config['general_input']['ideal_geometry']     = True
+    config['general_input']['backwards_raytrace'] = False
+    config['general_input']['do_visualizations']  = False
+    config['general_input']['do_savefiles']       = False
+    config['general_input']['do_image_analysis']  = False
+    config['general_input']['random_seed']        = 123456
+    config['general_input']['scenario']           = 'BEAM'
+    config['general_input']['system']             = 'w7x_ar16'
+    config['general_input']['shot']               = 180707017
 
-
-# -----------------------------------------------------------------------------
-## Load plasma properties
-config['plasma_input']['position']            = np.array([0, 0, 0])
-config['plasma_input']['normal']              = np.array([0, 1, 0])
-config['plasma_input']['orientation']         = np.array([0, 0, 1])
-config['plasma_input']['target']              = np.array([1, 0, 0])
-config['plasma_input']['width']               = 0.1
-config['plasma_input']['height']              = 0.1
-config['plasma_input']['depth']               = 0.1
-
-config['plasma_input']['space_resolution']    = 0.01 ** 3
-config['plasma_input']['time_resolution']     = 0.01
-config['plasma_input']['bundle_count']        = 100
-
-config['plasma_input']['spread']              = 2.0       #Angular spread (degrees)
-config['plasma_input']['temp']                = 1000      #Ion temperature (eV)
-config['plasma_input']['mass']                = 131.293   # Xenon mass (AMU)
-config['plasma_input']['wavelength']          = 2.7203    # Line location (angstroms)
-config['plasma_input']['linewidth']           = 1.129e+14 # Natural linewith (1/s)
-
-config['plasma_input']['volume_partitioning'] = False
+    # Temperature that the optical elements will be cooled to (kelvin)
+    config['general_input']['xics_temp'] = 273.0
 
 
-# -----------------------------------------------------------------------------
-## Load source properties
-# Xe44+ w line
-# Additional information on Xenon spectral lines can be found on nist.gov
-config['source_input']['intensity']           = 0
-config['source_input']['position']            = np.array([0, 0, 0])
-config['source_input']['normal']              = np.array([0, 1, 0])
-config['source_input']['orientation']         = np.array([0, 0, 1])
-config['source_input']['target']              = np.array([1, 0, 0])
+    # -----------------------------------------------------------------------------
+    ## Load plasma properties
+    config['plasma_input']['position']            = np.array([0, 0, 0])
+    config['plasma_input']['normal']              = np.array([0, 1, 0])
+    config['plasma_input']['orientation']         = np.array([0, 0, 1])
+    config['plasma_input']['target']              = np.array([1, 0, 0])
+    config['plasma_input']['width']               = 0.1
+    config['plasma_input']['height']              = 0.1
+    config['plasma_input']['depth']               = 0.1
 
-config['source_input']['spread']              = 1.0       #Angular spread (degrees)
-config['source_input']['temp']                = 1000      #Ion temperature (eV)
-config['source_input']['mass']                = 131.293   # Xenon mass (AMU)
-config['source_input']['wavelength']          = 2.7203    # Line location (angstroms)
-config['source_input']['linewidth']           = 1.129e+14 # Natural linewith (1/s)
+    config['plasma_input']['space_resolution']    = 0.01 ** 3
+    config['plasma_input']['time_resolution']     = 0.01
+    config['plasma_input']['bundle_count']        = 100
 
-#These values are arbitrary for now. Set to 0.0 for point source
-config['source_input']['width']               = 0.050
-config['source_input']['height']              = 0.050
-config['source_input']['depth']               = 0.050
+    config['plasma_input']['spread']              = 2.0       #Angular spread (degrees)
+    config['plasma_input']['temp']                = 1000      #Ion temperature (eV)
+    config['plasma_input']['mass']                = 131.293   # Xenon mass (AMU)
+    config['plasma_input']['wavelength']          = 2.7203    # Line location (angstroms)
+    config['plasma_input']['linewidth']           = 1.129e+14 # Natural linewith (1/s)
 
-
-# -----------------------------------------------------------------------------
-## Load spherical crystal properties
-
-# Rocking curve FWHM in radians
-# This is taken from x0h for quartz 1,1,-2,0
-# Darwin Curve, sigma: 48.070 urad
-# Darwin Curve, pi:    14.043 urad
-# Graphite Rocking Curve FWHM in radians
-# Taken from XOP: 8765 urad
-
-config['crystal_input']['position']           = [0.0, 0.0, 0.0]
-config['crystal_input']['normal']             = [0.0, 0.0, 0.0]
-config['crystal_input']['orientation']        = [0.0, 0.0, 0.0]
-
-config['crystal_input']['width']              = 0.040
-config['crystal_input']['height']             = 0.050
-config['crystal_input']['curvature']          = 1.200
-
-config['crystal_input']['spacing']            = 1.7059
-config['crystal_input']['reflectivity']       = 1
-config['crystal_input']['rocking_curve']      = 90.30e-6
-config['crystal_input']['pixel_scaling']      = int(200)
-
-config['crystal_input']['therm_expand']       = 5.9e-6
-#config['crystal_input']['sigma_data']         = '../xicsrt/rocking_curve_germanium_sigma.txt'
-#config['crystal_input']['pi_data']            = '../xicsrt/rocking_curve_germanium_pi.txt'
-config['crystal_input']['sigma_data']         = '/u/npablant/code/mirproject/xicsrt/xicsrt/rocking_curve_germanium_sigma.txt'
-config['crystal_input']['pi_data']            = '/u/npablant/code/mirproject/xicsrt/xicsrt/rocking_curve_germanium_pi.txt'
-config['crystal_input']['mix_factor']         = 1.0
-
-config['crystal_input']['do_bragg_checks']    = True
-config['crystal_input']['do_miss_checks']     = True
-config['crystal_input']['rocking_curve_type'] = "FILE"
+    config['plasma_input']['volume_partitioning'] = False
 
 
-# -----------------------------------------------------------------------------
-## Load mosaic graphite properties
+    # -----------------------------------------------------------------------------
+    ## Load source properties
+    # Xe44+ w line
+    # Additional information on Xenon spectral lines can be found on nist.gov
+    config['source_input']['intensity']           = 0
+    config['source_input']['position']            = np.array([0, 0, 0])
+    config['source_input']['normal']              = np.array([0, 1, 0])
+    config['source_input']['orientation']         = np.array([0, 0, 1])
+    config['source_input']['target']              = np.array([1, 0, 0])
 
-config['graphite_input']['position']          = [0.0, 0.0, 0.0]
-config['graphite_input']['normal']            = [0.0, 0.0, 0.0]
-config['graphite_input']['orientation']       = [0.0, 0.0, 0.0]
+    config['source_input']['spread']              = 1.0       #Angular spread (degrees)
+    config['source_input']['temp']                = 1000      #Ion temperature (eV)
+    config['source_input']['mass']                = 131.293   # Xenon mass (AMU)
+    config['source_input']['wavelength']          = 2.7203    # Line location (angstroms)
+    config['source_input']['linewidth']           = 1.129e+14 # Natural linewith (1/s)
 
-config['graphite_input']['width']             = 0.030
-config['graphite_input']['height']            = 0.040
-
-config['graphite_input']['reflectivity']      = 1
-config['graphite_input']['mosaic_spread']     = 0.5
-config['graphite_input']['spacing']           = 3.35
-config['graphite_input']['rocking_curve']     = 8765e-6
-config['graphite_input']['pixel_scaling']     = int(200)
-
-config['graphite_input']['therm_expand']      = 20e-6
-#config['graphite_input']['sigma_data']        = '../xicsrt/rocking_curve_graphite_sigma.txt'
-#config['graphite_input']['pi_data']           = '../xicsrt/rocking_curve_graphite_pi.txt'
-config['graphite_input']['sigma_data']        = '/u/npablant/code/mirproject/xicsrt/xicsrt/rocking_curve_graphite_sigma.txt'
-config['graphite_input']['pi_data']           = '/u/npablant/code/mirproject/xicsrt/xicsrt/rocking_curve_graphite_pi.txt'
-config['graphite_input']['mix_factor']        = 1.0
-
-config['graphite_input']['do_bragg_checks']   = True
-config['graphite_input']['do_miss_checks']    = True
-config['graphite_input']['rocking_curve_type']= "FILE"
+    #These values are arbitrary for now. Set to 0.0 for point source
+    config['source_input']['width']               = 0.050
+    config['source_input']['height']              = 0.050
+    config['source_input']['depth']               = 0.050
 
 
-# -----------------------------------------------------------------------------
-## Load detector properties
+    # -----------------------------------------------------------------------------
+    ## Load spherical crystal properties
 
-config['detector_input']['position']          = [0.0, 0.0, 0.0]
-config['detector_input']['normal']            = [0.0, 0.0, 0.0]
-config['detector_input']['orientation']       = [0.0, 0.0, 0.0]
+    # Rocking curve FWHM in radians
+    # This is taken from x0h for quartz 1,1,-2,0
+    # Darwin Curve, sigma: 48.070 urad
+    # Darwin Curve, pi:    14.043 urad
+    # Graphite Rocking Curve FWHM in radians
+    # Taken from XOP: 8765 urad
 
-config['detector_input']['pixel_size']        = 0.000172
-config['detector_input']['horizontal_pixels'] = 195
-config['detector_input']['vertical_pixels']   = 1475
-config['detector_input']['width']             = (config['detector_input']['horizontal_pixels']
-                                                * config['detector_input']['pixel_size'])
-config['detector_input']['height']            = (config['detector_input']['vertical_pixels']
-                                                * config['detector_input']['pixel_size'])
+    config['crystal_input']['position']           = [0.0, 0.0, 0.0]
+    config['crystal_input']['normal']             = [0.0, 0.0, 0.0]
+    config['crystal_input']['orientation']        = [0.0, 0.0, 0.0]
 
-config['detector_input']['do_miss_checks']    = True
+    config['crystal_input']['width']              = 0.040
+    config['crystal_input']['height']             = 0.050
+    config['crystal_input']['curvature']          = 1.200
+
+    config['crystal_input']['spacing']            = 1.7059
+    config['crystal_input']['reflectivity']       = 1
+    config['crystal_input']['rocking_curve']      = 90.30e-6
+    config['crystal_input']['pixel_scaling']      = int(200)
+
+    config['crystal_input']['therm_expand']       = 5.9e-6
+    #config['crystal_input']['sigma_data']         = '../xicsrt/rocking_curve_germanium_sigma.txt'
+    #config['crystal_input']['pi_data']            = '../xicsrt/rocking_curve_germanium_pi.txt'
+    config['crystal_input']['sigma_data']         = '/u/npablant/code/mirproject/xicsrt/xicsrt/rocking_curve_germanium_sigma.txt'
+    config['crystal_input']['pi_data']            = '/u/npablant/code/mirproject/xicsrt/xicsrt/rocking_curve_germanium_pi.txt'
+    config['crystal_input']['mix_factor']         = 1.0
+
+    config['crystal_input']['do_bragg_checks']    = True
+    config['crystal_input']['do_miss_checks']     = True
+    config['crystal_input']['rocking_curve_type'] = "FILE"
 
 
-# -----------------------------------------------------------------------------
-# Scenario properties
+    # -----------------------------------------------------------------------------
+    ## Load mosaic graphite properties
 
-# Each of these scenarios corresponds to a script located in xics_rt_tools.py
-# which assembles the optical elements into a specific configuration based on
-# input parameters
+    config['graphite_input']['position']          = [0.0, 0.0, 0.0]
+    config['graphite_input']['normal']            = [0.0, 0.0, 0.0]
+    config['graphite_input']['orientation']       = [0.0, 0.0, 0.0]
 
-# Load scenario properties
-config['scenario_input']['source_graphite_dist']  = 2
-config['scenario_input']['graphite_crystal_dist'] = 8.5
-config['scenario_input']['crystal_detector_dist'] = None
-config['scenario_input']['graphite_offset']       = np.array([0,0,0], dtype = np.float64)
-config['scenario_input']['graphite_tilt']         = np.array([0,0,0], dtype = np.float64)
-config['scenario_input']['crystal_offset']        = np.array([0,0,0], dtype = np.float64)
-config['scenario_input']['crystal_tilt']          = np.array([0,0,0], dtype = np.float64)
-config['scenario_input']['detector_offset']       = np.array([0,0,0], dtype = np.float64)
-config['scenario_input']['detector_tilt']         = np.array([0,0,0], dtype = np.float64)
+    config['graphite_input']['width']             = 0.030
+    config['graphite_input']['height']            = 0.040
 
+    config['graphite_input']['reflectivity']      = 1
+    config['graphite_input']['mosaic_spread']     = 0.5
+    config['graphite_input']['spacing']           = 3.35
+    config['graphite_input']['rocking_curve']     = 8765e-6
+    config['graphite_input']['pixel_scaling']     = int(200)
+
+    config['graphite_input']['therm_expand']      = 20e-6
+    #config['graphite_input']['sigma_data']        = '../xicsrt/rocking_curve_graphite_sigma.txt'
+    #config['graphite_input']['pi_data']           = '../xicsrt/rocking_curve_graphite_pi.txt'
+    config['graphite_input']['sigma_data']        = '/u/npablant/code/mirproject/xicsrt/xicsrt/rocking_curve_graphite_sigma.txt'
+    config['graphite_input']['pi_data']           = '/u/npablant/code/mirproject/xicsrt/xicsrt/rocking_curve_graphite_pi.txt'
+    config['graphite_input']['mix_factor']        = 1.0
+
+    config['graphite_input']['do_bragg_checks']   = True
+    config['graphite_input']['do_miss_checks']    = True
+    config['graphite_input']['rocking_curve_type']= "gaussian"
+
+
+    # -----------------------------------------------------------------------------
+    ## Load detector properties
+
+    config['detector_input']['position']          = [0.0, 0.0, 0.0]
+    config['detector_input']['normal']            = [0.0, 0.0, 0.0]
+    config['detector_input']['orientation']       = [0.0, 0.0, 0.0]
+
+    config['detector_input']['pixel_size']        = 0.000172
+    config['detector_input']['horizontal_pixels'] = 195
+    config['detector_input']['vertical_pixels']   = 1475
+    config['detector_input']['width']             = (config['detector_input']['horizontal_pixels']
+                                                    * config['detector_input']['pixel_size'])
+    config['detector_input']['height']            = (config['detector_input']['vertical_pixels']
+                                                    * config['detector_input']['pixel_size'])
+
+    config['detector_input']['do_miss_checks']    = True
+
+
+    # -----------------------------------------------------------------------------
+    # Scenario properties
+
+    # Each of these scenarios corresponds to a script located in xics_rt_tools.py
+    # which assembles the optical elements into a specific configuration based on
+    # input parameters
+
+    # Load scenario properties
+    config['scenario_input']['source_graphite_dist']  = 2
+    config['scenario_input']['graphite_crystal_dist'] = 8.5
+    config['scenario_input']['crystal_detector_dist'] = None
+    config['scenario_input']['graphite_offset']       = np.array([0,0,0], dtype = np.float64)
+    config['scenario_input']['graphite_tilt']         = np.array([0,0,0], dtype = np.float64)
+    config['scenario_input']['crystal_offset']        = np.array([0,0,0], dtype = np.float64)
+    config['scenario_input']['crystal_tilt']          = np.array([0,0,0], dtype = np.float64)
+    config['scenario_input']['detector_offset']       = np.array([0,0,0], dtype = np.float64)
+    config['scenario_input']['detector_tilt']         = np.array([0,0,0], dtype = np.float64)
+
+    return config
 
 
 
--- xicsrt/xics_rt_raytrace.py
+++ xicsrt/xics_rt_raytrace.py
@@ -9,14 +9,15 @@ from copy import deepcopy
 
 from xicsrt.util import profiler
 
-def raytrace(source, detector, *optics,  number_of_runs=None, collect_optics=None):
+from xicsrt.xics_rt_objects import RayArray
+
+def raytrace_single(source, detector, *optics,  number_of_runs=None, collect_optics=None):
     """ 
     Rays are generated from source and then passed through the optics in
     the order listed. Finally, they are collected by the detector. 
     Rays consists of origin, direction, wavelength, and weight.
     """
-    
-    if number_of_runs is None: number_of_runs = 1
+
     if collect_optics is None: collect_optics = False
     
     rays_count = dict()
@@ -24,47 +25,49 @@ def raytrace(source, detector, *optics,  number_of_runs=None, collect_optics=Non
     rays_count['total_graphite']   = 0
     rays_count['total_crystal']    = 0
     rays_count['total_detector']   = 0
-    
-    for ii in range(number_of_runs):
-        profiler.start('Raytrace Run')
-        print('')
-        print('Starting iteration: {} of {}'.format(ii + 1, number_of_runs))
-        
-        # Rays history resets after every run and only returns on the last one
-        rays_history = []
-            
-        profiler.start('Ray Generation')
-        rays = source.generate_rays()
+
+    profiler.start('Raytrace Run')
+
+    # Rays history resets after every run and only returns on the last one
+    rays_history = []
+
+    profiler.start('Ray Generation')
+    rays = source.generate_rays()
+    profiler.stop('Ray Generation')
+    rays_history.append(deepcopy(rays))
+
+
+    print(' Rays Generated:    {:6.4e}'.format(rays['direction'].shape[0]))
+    rays_count['total_generated'] += rays['direction'].shape[0]
+
+    for optic in optics:
+        profiler.start('Ray Tracing')
+        rays = optic.light(rays)
+        profiler.stop('Ray Tracing')
         rays_history.append(deepcopy(rays))
-        profiler.stop('Ray Generation')
-                
-        print(' Rays Generated:    {:6.4e}'.format(rays['direction'].shape[0]))
-        rays_count['total_generated'] += rays['direction'].shape[0]
 
-        for optic in optics:
-            profiler.start('Ray Tracing')
-            rays = optic.light(rays)
-            rays_history.append(deepcopy(rays))
-            profiler.stop('Ray Tracing')
+        profiler.start('Collection: Optics')
+        if collect_optics:
+            optic.collect_rays(rays)
+        profiler.stop('Collection: Optics')
 
-            profiler.start('Collection: Optics')
-            if collect_optics:
-                optic.collect_rays(rays)
-            profiler.stop('Collection: Optics')
-            
-            if optic.__name__ == 'SphericalCrystal':
-                rays_count['total_crystal'] += optic.photon_count
-            elif optic.__name__ == 'MosaicGraphite':
-                rays_count['total_graphite'] += optic.photon_count
+        if optic.__name__ == 'SphericalCrystal':
+            rays_count['total_crystal'] += optic.photon_count
+        elif optic.__name__ == 'MosaicGraphite':
+            rays_count['total_graphite'] += optic.photon_count
 
-        profiler.start('Collection: Detector')
-        rays = detector.light(rays)
-        rays_history.append(deepcopy(rays))
-        detector.collect_rays(rays)
-        profiler.stop('Collection: Detector')
-        
-        rays_count['total_detector'] += detector.photon_count
-        profiler.stop('Raytrace Run')
+    profiler.start('Ray Tracing')
+    rays = detector.light(rays)
+    profiler.stop('Ray Tracing')
+    rays_history.append(deepcopy(rays))
+
+    profiler.start('Collection: Detector')
+    detector.collect_rays(rays)
+    profiler.stop('Collection: Detector')
+
+    rays_count['total_detector'] += detector.photon_count
+    profiler.stop('Raytrace Run')
+
 
     print('')
     print('Total Rays Generated: {:6.4e}'.format(rays_count['total_generated']))
@@ -78,5 +81,82 @@ def raytrace(source, detector, *optics,  number_of_runs=None, collect_optics=Non
     print('')
 
     return rays_history, rays_count
+
+
+def raytrace(source, detector, *optics, number_of_runs=None, collect_optics=None):
+    """
+    Run a series of ray tracing runs.  Save all rays that make it to the detector
+    and a subset of rays that are lost.
+    """
+
+    print('Using local raytrace.')
+
+    if number_of_runs is None: number_of_runs = 1
+
+    count = dict()
+    count['total_generated']  = 0
+    count['total_graphite']   = 0
+    count['total_crystal']    = 0
+    count['total_detector']   = 0
+
+    history = {}
+    history['found'] = []
+    history['lost'] = []
+
+    for ii in range(number_of_runs):
+        print('')
+        print('Starting iteration: {} of {}'.format(ii + 1, number_of_runs))
+
+        history_temp, count_temp = raytrace_single(source, detector, *optics, collect_optics=collect_optics)
+
+        for key in count_temp:
+            count[key] += count_temp[key]
+
+        w_found = np.flatnonzero(history_temp[-1]['mask'])
+        w_lost = np.flatnonzero(np.invert(history_temp[-1]['mask']))
+        max_lost = 5000
+        num_lost = min(max_lost, len(w_lost))
+
+        found = []
+        lost = []
+        for ii_opt in range(len(history_temp)):
+            found.append({})
+            lost.append({})
+
+            for key in history_temp[ii_opt]:
+                found[ii_opt][key] = history_temp[ii_opt][key][w_found]
+                lost[ii_opt][key] = history_temp[ii_opt][key][w_lost[:num_lost]]
+
+        history['found'].append(found)
+        history['lost'].append(lost)
+
+    # Now combine all the histories.
+    ray_temp = RayArray()
+    ray_temp.zeros(0)
+
+    history_final = {}
+    history_final['found'] = []
+    history_final['lost'] = []
+    for ii_opt in range(len(history['found'][0])):
+        history_final['found'].append(ray_temp.copy())
+        history_final['lost'].append(ray_temp.copy())
+
+    for ii_run in range(len(history['found'])):
+        for ii_opt in range(len(history['found'][0])):
+            history_final['found'][ii_opt].extend(history['found'][ii_run][ii_opt])
+            history_final['lost'][ii_opt].extend(history['lost'][ii_run][ii_opt])
+
+    print('')
+    print('Final Rays Generated: {:6.4e}'.format(count['total_generated']))
+    print('Final Rays on HOPG:   {:6.4e}'.format(count['total_graphite']))
+    print('Final Rays on Crystal:{:6.4e}'.format(count['total_crystal']))
+    print('Final Rays Detected:  {:6.4e}'.format(count['total_detector']))
+    print('Efficiency: {:6.2e} ± {:3.1e} ({:7.5f}%)'.format(
+        count['total_detector'] / count['total_generated'],
+        np.sqrt(count['total_detector']) / count['total_generated'],
+        count['total_detector'] / count['total_generated'] * 100))
+    print('')
+
+    return history_final, count
 
 
