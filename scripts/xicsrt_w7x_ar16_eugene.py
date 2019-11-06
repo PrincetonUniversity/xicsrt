Fixed plasma solid angle calculation
Plasma should now calculate intensity from emissivity correctly.

scripts/xicsrt_w7x_ar16_eugene.py
@@ -117,10 +117,10 @@ plasma_input['major_radius']        = 6.2
 plasma_input['minor_radius']        = 2.0
        
 plasma_input['space_resolution']    = 0.01 ** 3
-plasma_input['time_resolution']     = 0.01
+plasma_input['time_resolution']     = 1
 plasma_input['bundle_count']        = 10000
 
-plasma_input['spread']              = 2.0       #Angular spread (degrees)
+plasma_input['spread']              = 1         #Angular spread (degrees)
 plasma_input['temp']                = 1000      #Ion temperature (eV)
 plasma_input['mass']                = 131.293   # Xenon mass (AMU)
 plasma_input['wavelength']          = 2.7203    # Line location (angstroms)
@@ -239,7 +239,7 @@ scenario_input['sagitt_focus']      = - scenario_input['meridi_focus'] / np.cos(
                                       2 * scenario_input['crystal_bragg'])
 
 # Load scenario properties
-scenario_input['source_graphite_dist']  = 20
+scenario_input['source_graphite_dist']  = 1
 scenario_input['graphite_crystal_dist'] = 8.5
 scenario_input['crystal_detector_dist'] = scenario_input['meridi_focus']
 scenario_input['graphite_offset']       = np.array([0,0,0], dtype = np.float64)
@@ -262,7 +262,8 @@ scenario_input['detector_input']        = detector_input
 scenario_input['effective_width_mm']    = (crystal_input['curvature'] * 
               graphite_input['height'] * np.sin(scenario_input['graphite_bragg']
               ) / (scenario_input['graphite_crystal_dist'] - 1)) * 1000
-scenario_input['effective_width_px']    = int(scenario_input['effective_width_mm'] * 1000 / crystal_input['pixel_scaling'])
+scenario_input['effective_width_px']    = int(scenario_input['effective_width_mm']
+                 * 1000 / crystal_input['pixel_scaling'])
 
 ## Set up a legacy beamline scenario ------------------------------------------
 if general_input['scenario'] == 'LEGACY':
@@ -274,7 +275,7 @@ if general_input['scenario'] == 'LEGACY':
     )
     
     source_input['target'] = crystal_input['position']
-    source_input['normal'] = (crystal_input['position'] - source_input['position'])
+    source_input['normal'] = crystal_input['position'] - source_input['position']
     source_input['normal']/=  np.linalg.norm(source_input['normal'])
 
     #This direction is rather abitrary and is not (in general)

xicsrt/xics_rt_plasmas.py
@@ -32,15 +32,15 @@ class GenericPlasma(TraceObject):
         self.position       = plasma_input['position']
         self.normal         = plasma_input['normal']
         self.xorientation   = plasma_input['orientation']
-        self.yorientation   = (np.cross(self.normal, self.xorientation) / 
-                               np.linalg.norm(np.cross(self.normal, self.xorientation)))
+        self.yorientation   = np.cross(self.normal, self.xorientation )
+        self.yorientation  /= np.linalg.norm(self.yorientation)
         self.target         = plasma_input['target']
         # Voxels are 3D pixels, Chronons are time pixels
         self.width          = plasma_input['width']
         self.height         = plasma_input['height']
         self.depth          = plasma_input['depth']
         self.volume         = self.width * self.height * self.depth
-        self.solid_angle    = plasma_input['spread'] * (np.pi ** 2) / 180 
+        self.solid_angle    = 4 * np.pi * np.sin(plasma_input['spread'] * np.pi / 360) ** 2
         self.voxel_size     = plasma_input['space_resolution']
         self.chronon_size   = plasma_input['time_resolution']
         self.bundle_count   = plasma_input['bundle_count']
@@ -84,7 +84,8 @@ class GenericPlasma(TraceObject):
             source_input['position']    = bundle_input['position'][ii]
             source_input['temp']        = bundle_input['temp'][ii]
             source_input['intensity']   = int(bundle_input['emissivity'][ii]
-                * self.chronon_size * self.voxel_size * self.solid_angle)
+                * self.chronon_size * self.voxel_size 
+                * self.solid_angle / (4 * np.pi))
             
             #constants
             source_input['width'], source_input['height'], source_input['depth'] = [np.power(self.voxel_size, 1/3)] * 3
@@ -253,8 +254,8 @@ class ToroidalPlasma(GenericPlasma):
         
         #evaluate emissivity at each point
         #plasma torus emissivity falls off as a function of radius
-        bundle_input['emissivity'][step_test]   = 1e11
-  
+        bundle_input['emissivity'][step_test]   = 1e12
+        
         return bundle_input
 
     def generate_rays(self):