a5e3cbf4acf220ad8353b93b0658ddfb88eddf4a
Novimir Antoniuk Pablant <novimir.pablant@amicitas.com>
2020-03-04 14:58:34 -0500

    Added in the BundleFilters.

---
+++ xicsrt/filters/_XicsrtBundleFilter.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Authors
+-------
+- Novimir A. Pablant <npblant@pppl.gov>
+"""
+
+import numpy as np
+
+from xicsrt.xics_rt_objects import ConfigObject
+
+class XicsrtBundleFilter(ConfigObject):
+    """
+    A base class for bundle filters.
+    """
+
+    def __init__(self, config=None):
+        self.config = self.getDefaultConfig()
+        self.updateConfig(config)
+    
+    def getDefaultConfig(self):
+        config = super().getDefaultConfig()
+        return config
+    
+    def filter(self, bundle_input):
+        return bundle_input
---
+++ xicsrt/filters/_XicsrtBundleFilterSightline.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Authors
+-------
+- Novimir A. Pablant <npblant@pppl.gov>
+
+"""
+
+import numpy as np
+from collections import OrderedDict
+
+from xicsrt.filters._XicsrtBundleFilter import XicsrtBundleFilter
+
+class XicsrtBundleFilterSightline(XicsrtBundleFilter):
+    """
+    A bundle filter based on proximity to sightline vectors.
+    """
+
+    def __init__(self, config=None):
+        super().__init__(config)    
+        
+    def getDefaultConfig(self):
+        config = super().getDefaultConfig()
+
+        config['origin'] = None
+        config['direction'] = None
+        config['radius'] = None
+
+        return config
+
+    def filter(self, bundle_input):
+
+        # Calculate whether the ray bundles are within range of the sightline
+        # vector from sightline origin to bundle position.
+        l_0 = self.config['origin'] - bundle_input['position']
+        
+        # Projection of l_0 onto the sightline
+        proj = np.einsum('j,ij->i', self.config['direction'], l_0)[np.newaxis]
+        l_1  = np.dot(np.transpose(self.config['direction'][np.newaxis]), proj)
+        l_1  = np.transpose(l_1)
+        
+        # Component of l_0 perpendicular to the sightline
+        l_2 = l_0 - l_1
+        
+        # Sightline distance is the length of l_2
+        distance = np.einsum('ij,ij->i', l_2, l_2) ** .5
+        
+        # Check to see if the bundle is close enough to the sightline
+        mask = (self.config['radius'] >= distance)
+
+        bundle_input['mask'] &= mask
+
+        return bundle_input
