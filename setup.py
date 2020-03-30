ea45af1744cc9abb7f812cba9603543d710a5647
Novimir Antoniuk Pablant <novimir.pablant@amicitas.com>
2020-03-30 16:16:22 -0400

    Added a basic setup script. Added a basic command line script.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README') as ff:
+    long_description = ff.read()
+
+exec(open('xicsrt/_version.py').read())
+
+classifiers = [
+    "Development Status :: 4 - Beta"
+    ,"License :: OSI Approved :: MIT License"
+    ,"Programming Language :: Python :: 3"
+    ,"Operating System :: OS Independent"
+    ]
+
+params ={
+    'name':'xicsrt'
+    ,'version':__version__
+    ,'author':'Novimir Antoniuk Pablant'
+    ,'author_email':'npablant@pppl.gov'
+    ,'description':'A simple raytracing application written in Python.'
+    ,'long_description':long_description
+    ,'url':'http://amicitas.bitbucket.org/xicsrt/'
+    ,'license':'MIT'
+    ,'packages':setuptools.find_packages()
+    ,'classifiers':classifiers
+    ,'install_requires':['numpy', 'pillow']
+    ,'python_requires':'>=3.5'
+    }
+    
+setuptools.setup(**params)
---
+++ xicsrt/xicsrt.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Authors:
+  | Novimir Antoniuk Pablant <npablant@pppl.gov>
+
+Purpose:
+  A command line interface for the XICSRT raytracer.
+"""
+
+import numpy as np
+import argparse
+
+from xicsrt import xicsrt_raytrace
+from xicsrt import xicsrt_input
+
+def run():
+
+    global parser
+    global args
+
+    parser = argparse.ArgumentParser(
+"""
+Perform an XICSRT raytrace.
+
+The input to this command should be an XICSRT configuration dictionary
+in json format.
+
+Example:
+python xicsrt.py config.json
+
+""") 
+
+    parser.add_argument('config_file'
+                        ,type=str
+                        ,nargs=1
+                        ,default='config.json'
+                        ,help='The path to the configuration file for this run.')
+
+    args = parser.parse_args()
+    config = xicsrt_input.load_config(args.config_file)
+    result = xicsrt_raytrace.xicsrt_raytrace.raytrace_multi(config)
+
+if __name__ == "__main__":
+    run()
