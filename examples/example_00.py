4d097e19e00733b43bf8e729008579f2a31f204f
Novimir Antoniuk Pablant <novimir.pablant@amicitas.com>
2020-03-30 11:50:39 -0400

    Updated the example.

--- examples/example_00.py
+++ examples/example_00.py
@@ -10,26 +10,21 @@ Description:
 import numpy as np
 from collections import OrderedDict
 
-# Setup the module path.
-import sys
-sys.path.append('/u/npablant/code/mirproject/xicsrt')
-
 # Import xicsrt modules
 from xicsrt import xicsrt_raytrace
 
 config = OrderedDict()
 
 config['general'] = OrderedDict()
-config['general']['number_of_runs'] = 5
-config['general']['class_pathlist'] = [
-    '/u/npablant/code/mirproject/xicsrt/xicsrt/sources'
-    ,'/u/npablant/code/mirproject/xicsrt/xicsrt/optics'
-    ]
+config['general']['number_of_iter'] = 5
+
+config['general']['output_path'] = ''
+config['general']['save_images'] = False
 
 config['sources'] = OrderedDict()
 config['sources']['source'] = OrderedDict()
 config['sources']['source']['class_name'] = 'XicsrtSourceDirected'
-config['sources']['source']['intensity'] = 10
+config['sources']['source']['intensity'] = 1e4
 config['sources']['source']['wavelength'] = 3.9492
 config['sources']['source']['spread'] = np.radians(0.1)
 
@@ -38,7 +33,7 @@ config['optics']['crystal'] = OrderedDict()
 config['optics']['crystal']['class_name'] = 'XicsrtOpticCrystalSpherical'
 config['optics']['crystal']['do_miss_check'] = True
 config['optics']['crystal']['origin'] = [0.0,0.0,1.0]
-config['optics']['crystal']['zaxis'] = [0.00000000, -0.59497864, -0.80374151]
+config['optics']['crystal']['zaxis'] = [0.00000000, 0.59497864, -0.80374151]
 config['optics']['crystal']['width']  = 0.001
 config['optics']['crystal']['height'] = 0.001
 config['optics']['crystal']['radius'] = 1.0
@@ -52,4 +47,3 @@ config['optics']['crystal']['rocking_type'] = 'gaussian'
 config['optics']['crystal']['rocking_fwhm'] = 48.070e-6
 
 output = xicsrt_raytrace.raytrace(config)
-xicsrt_raytrace.print_raytrace(output)
