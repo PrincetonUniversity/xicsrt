Converted angles in config to radians. Converted OrderedDict to dict in a few places. Revived the Toroidal plasmas. Other small bug fixes and improvements.

examples/example_00.py
@@ -13,23 +13,23 @@ from collections import OrderedDict
 # Import xicsrt modules
 from xicsrt import xicsrt_raytrace
 
-config = OrderedDict()
+config = dict()
 
-config['general'] = OrderedDict()
+config['general'] = dict()
 config['general']['number_of_iter'] = 5
 
 config['general']['output_path'] = ''
 config['general']['save_images'] = False
 
-config['sources'] = OrderedDict()
-config['sources']['source'] = OrderedDict()
+config['sources'] = dict()
+config['sources']['source'] = dict()
 config['sources']['source']['class_name'] = 'XicsrtSourceDirected'
 config['sources']['source']['intensity'] = 1e4
 config['sources']['source']['wavelength'] = 3.9492
 config['sources']['source']['spread'] = np.radians(0.1)
 
-config['optics'] = OrderedDict()
-config['optics']['crystal'] = OrderedDict()
+config['optics'] = dict()
+config['optics']['crystal'] = dict()
 config['optics']['crystal']['class_name'] = 'XicsrtOpticCrystalSpherical'
 config['optics']['crystal']['do_miss_check'] = True
 config['optics']['crystal']['origin'] = [0.0,0.0,1.0]

xicsrt/optics/_XicsrtOpticMosaicGraphite.py
@@ -76,7 +76,7 @@ class XicsrtOpticMosaicGraphite(XicsrtOpticCrystal):
         # average graphite mirror norm
         m = mask
 
-        rad_spread = np.radians(self.param['mosaic_spread'])
+        rad_spread = self.param['mosaic_spread']
         dir_local = xicsrt_math.vector_dist_gaussian(rad_spread, np.sum(m))
 
         R = np.empty((np.sum(m), 3, 3,), dtype=np.float64)

xicsrt/sources/_XicsrtPlasmaGeneric.py
@@ -67,7 +67,7 @@ class XicsrtPlasmaGeneric(GeometryObject):
         self.param['bundle_type']  = str.lower(self.param['bundle_type'])
         self.param['bundle_count'] = int(self.param['bundle_count'])
         self.param['volume']       = self.config['width'] * self.config['height'] * self.config['depth']
-        self.param['solid_angle']  = 4 * np.pi * np.sin(np.radians(self.config['spread'])/2)**2
+        self.param['solid_angle']  = 4 * np.pi * np.sin(self.config['spread']/2)**2
         
         
     def setup_bundles(self):
@@ -92,6 +92,15 @@ class XicsrtPlasmaGeneric(GeometryObject):
 
         bundle_input['origin'][:] = self.point_to_external(offset)
         return bundle_input
+
+    def get_emissivity(self, rho):
+        return self.param['emissivity']
+
+    def get_temperature(self, rho):
+        return self.param['temperature']
+
+    def get_velocity(self, rho):
+        return self.param['velocity']
 
     def bundle_generate(self, bundle_input):
         return bundle_input

xicsrt/sources/_XicsrtPlasmaToroidal.py
@@ -6,51 +6,65 @@ Authors
   - Yevgeniy Yakusevich <eugenethree@gmail.com>
 """
 import logging
-import numpy as np   
-from collections import OrderedDict
+import numpy as np
 
 from xicsrt.util import profiler
 from xicsrt.sources._XicsrtPlasmaGeneric import XicsrtPlasmaGeneric
-from xicsrt.tools.xicsrt_math    import cyl_from_car, tor_from_car
+
+import xicsrt.tools.xicsrt_math as xm
+
 
 class  XicsrtPlasmaToroidal(XicsrtPlasmaGeneric):
     """
-    A plasma object with toroidal geometery.
-
-    This class is meant only to be used as an exmple for generating 
-    more complecated classes for specific plasmas.
+    A plasma object with toroidal geometry and a circular
+    cross-section.
     """
         
     def get_default_config(self):
         config = super().get_default_config()
         config['major_radius'] = 0.0
         config['minor_radius'] = 0.0
+        config['emissivity_scale']  = 1.0
+        config['temperature_scale'] = 1.0
+        config['velocity_scale']    = 1.0
         return config
-        
-    def bundle_generate(self, bundle_input):
-        #create a long list containing random points within the cube's dimensions
-        x_offset = np.random.uniform(-1 * self.param['width']/2 , self.param['width']/2,  self.param['bundle_count'])
-        y_offset = np.random.uniform(-1 * self.param['height']/2, self.param['height']/2, self.param['bundle_count'])
-        z_offset = np.random.uniform(-1 * self.param['depth']/2 , self.param['depth']/2,  self.param['bundle_count'])
-        
-        #unlike the other plasmas, the toroidal plasma has fixed orientation to
-        #prevent confusion
-        bundle_input['origin'][:] = (
-            self.origin
-            + np.einsum('i,j', x_offset, np.array([1, 0, 0]))
-            + np.einsum('i,j', y_offset, np.array([0, 1, 0]))
-            + np.einsum('i,j', z_offset, np.array([0, 0, 1])))
-        
-        #convert from cartesian coordinates to toroidal coordinates [sigma, tau, phi]
-        #torus is oriented along the Z axis
-        rad, pol, tor = tor_from_car(x_offset, y_offset, z_offset, self.param['major_radius'])
-        
-        step_test = (rad <= self.param['minor_radius'])
 
-        # Let plasma temperature and emissivity fall off as a function of
-        # radius.
-        bundle_input['emissivity'][step_test]  = self.param['emissivity'] / radius
-        bundle_input['temperature'][step_test] = self.param['temperature'] / radius
-        bundle_input['velocity'][step_test]    = self.param['velocity']
-        
+    def flx_from_car(self, point_car):
+        flx = xm.tor_from_car(point_car, self.param['major_radius'])
+        return flx
+
+    def rho_from_car(self, point_car):
+        point_flx = self.flx_from_car(point_car)
+        return np.sqrt(point_flx[0])
+
+    def bundle_generate(self, bundle_input):
+
+        profiler.start("Bundle Input Generation")
+        m = bundle_input['mask']
+
+        print('sum(m)', sum(m))
+
+        # Attempt to generate the specified number of bundles, but throw out
+        # bundles that our outside of the last closed flux surface.
+        #
+        # This loop was setup for VMEC. Here we could do this as a single
+        # vectorized operation instead.
+        rho = np.zeros(len(m[m]))
+        for ii in range(len(m[m])):
+            # convert from cartesian coordinates to normalized radial coordinate.
+            profiler.start("Fluxspace from Realspace")
+            rho[ii] = self.rho_from_car(bundle_input['origin'][m][ii, :])
+            profiler.stop("Fluxspace from Realspace")
+
+        # evaluate emissivity, temperature and velocity at each bundle location.
+        bundle_input['temperature'][m] = self.get_temperature(rho) * self.param['temperature_scale']
+        bundle_input['emissivity'][m] = self.get_emissivity(rho) * self.param['emissivity_scale']
+        bundle_input['velocity'][m] = self.get_velocity(rho) * self.param['velocity_scale']
+
+        fintest = np.isfinite(bundle_input['temperature'])
+
+        m &= fintest
+
+        profiler.stop("Bundle Input Generation")
+
         return bundle_input

xicsrt/sources/_XicsrtSourceGeneric.py
@@ -109,7 +109,7 @@ class XicsrtSourceGeneric(GeometryObject):
 
     def random_direction(self, normal):
 
-        rad_spread = np.radians(self.param['spread'])
+        rad_spread = self.param['spread']
         dir_local  = xicsrt_math.vector_dist_uniform(rad_spread, self.param['intensity'])
 
         # Generate some basis vectors that are perpendicular

xicsrt/tools/xicsrt_math.py
@@ -82,7 +82,7 @@ def vector_dist_gaussian(FWHM, number):
     # convert the angular sigma into a linear-displacement-from-vertical
     sigma_z = 1 - np.cos(sigma)
     # create the half-normal distribution of vertical displacement.
-    dist = abs(np.random.normal(0, sigma_z, 10))
+    dist = abs(np.random.normal(0, sigma_z, number))
     z = 1.0 - dist
 
     phi = np.random.uniform(0, 2 * np.pi, number)
@@ -93,24 +93,24 @@ def vector_dist_gaussian(FWHM, number):
 
     return output
 
-def cyl_from_car(x, y, z):
+def cyl_from_car(point_car):
     #convert cartesian coordinates -> cylindirical coordinates
-    radius  = np.sqrt(np.power(x,2) + np.power(y,2))
-    azimuth = np.arctan2(y, x)
-    height  = z
-    
-    return radius, azimuth, height
+    radius  = np.sqrt(np.power(point_car[0],2) + np.power(point_car[1],2))
+    azimuth = np.arctan2(point_car[1], point_car[0])
+    height  = point_car[2]
+    point_cyl = [radius, azimuth, height]
+    return point_cyl
 
-def tor_from_car(x, y, z, a):
+def tor_from_car(point_car, a):
     """
-    X Y Z       = Cartesian Coordinates
-    rho         = Cylindrical Radius
-    a           = Tokamak Major Radius
-    rad pol tor = Toroidal Radius, Poloidal Angle, Toroidal Angle
+    point_car   = Cartesian Coordinates [x,y,z]
+    a           = Torus Major Radius
+    point_tor   = [Minor Radius, Poloidal Angle, Toroidal Angle]
     """
-    rho  = np.sqrt(np.power(x,2) + np.power(y,2))
-    tor  = np.arctan2(y, x) + np.pi
+    rho  = np.sqrt(np.power(point_car[0],2) + np.power(point_car[1],2))
+    tor  = np.arctan2(point_car[1], point_car[0]) + np.pi
     w    = rho - a
-    pol  = np.arctan2(z, w)
-    rad  = np.sqrt(np.power(z,2) + np.power(w,2))
-    return rad, pol, tor
+    pol  = np.arctan2(point_car[2], w)
+    rad  = np.sqrt(np.power(point_car[2],2) + np.power(w,2))
+    point_tor = np.asarray([rad, pol, tor])
+    return point_tor