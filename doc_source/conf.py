1341009cb7e4d9e9cf2d0fc730ded8b267da529b
Novimir Antoniuk Pablant <novimir.pablant@amicitas.com>
2020-10-01 14:52:50 -0400

    Updated the sphinx config.

--- doc_source/conf.py
+++ doc_source/conf.py
@@ -26,11 +26,12 @@ release = '0.3.4'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 import sphinx_rtd_theme
-
 extensions = [
     'sphinx.ext.autodoc'
     ,'sphinx.ext.napoleon'
