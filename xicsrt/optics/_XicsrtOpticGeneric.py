cab81b6b146b284382688759ca0fc8de5bed7ae4
bartlettnbb2 <bartlettnbb2@gmail.com>
2021-03-10 17:59:09 -0600

    *New aperture optic object created *intersect modified to handle local coordinate change *intersect_check modified to handle local coordinate change

--- xicsrt/optics/_XicsrtOpticGeneric.py
+++ xicsrt/optics/_XicsrtOpticGeneric.py
@@ -37,6 +37,14 @@ class XicsrtOpticGeneric(GeometryObject):
           The size of this element along the zaxis direction.
           Typically not required, but if needed will correspond to the 'depth'
           of the optic.
+          
+        opt_size:  numpy array (np.array([[0,0],[0],[0]]))
+          The size of the actual optical elements. All rays hitting outside
+          the optical element will be masked. The entire optical elemnt will
+          behave like a detector.
+          
+        opt_size
+          The size of the actual optical instrument that interacts with rays
 
         pixel_size: float (None)
           The pixel size, used for binning rays into images.
@@ -64,6 +72,7 @@ class XicsrtOpticGeneric(GeometryObject):
         config['xsize']          = 0.0
         config['ysize']          = 0.0
         config['zsize']          = 0.0
+        config['opt_size']       = None
         config['pixel_size']     = None
 
         # boolean settings
@@ -135,12 +144,13 @@ class XicsrtOpticGeneric(GeometryObject):
         depending on the how the optic is designed and the value
         of the configuration option: 'do_trace_local'.
 
-        This method can be re-implemented by individual optics.
+        This method can be re-implemented by indiviual optics.
         """
         m = rays['mask']
 
         distance = self.intersect(rays)
         X, rays  = self.intersect_check(rays, distance)
+        self.aperture(X, rays, m)
         self.log.debug(' Rays on {}:   {:6.4e}'.format(self.name, m[m].shape[0]))
         normals  = self.generate_normals(X, rays)
         rays     = self.reflect_vectors(X, rays, normals)
@@ -174,10 +184,14 @@ class XicsrtOpticGeneric(GeometryObject):
         D = rays['direction']
         m = rays['mask']
         
-        distance = np.zeros(m.shape, dtype=np.float64)
-        distance[m] = (np.dot((self.param['origin'] - O[m]), self.param['zaxis'])
-                       / np.dot(D[m], self.param['zaxis']))
-
+        if self.param['do_trace_local']:
+            distance = np.zeros(m.shape, dtype=np.float64)
+            distance[m] = (np.dot((0 - O[m]), np.array([ 0., 0., 1.]))
+                           / np.dot(D[m], np.array([ 0., 0., 1.])))
+        else:
+            distance = np.zeros(m.shape, dtype=np.float64)
+            distance[m] = (np.dot((self.param['origin'] - O[m]), self.param['zaxis'])
+                           / np.dot(D[m], self.param['zaxis']))
         # Update the mask to only include positive distances.
         m &= (distance >= 0)
 
@@ -198,12 +212,30 @@ class XicsrtOpticGeneric(GeometryObject):
 
         X_local[m] = self.point_to_local(X[m])
         
+        if self.param['do_trace_local']:
         # Find which rays hit the optic, update mask to remove misses
-        if self.param['do_miss_check'] is True:
-            m[m] &= (np.abs(X_local[m,0]) < self.param['xsize'] / 2)
-            m[m] &= (np.abs(X_local[m,1]) < self.param['ysize'] / 2)
-
+            if self.param['do_miss_check'] is True:
+                m[m] &= (np.abs(X[m,0]) < self.param['xsize'] / 2)
+                m[m] &= (np.abs(X[m,1]) < self.param['ysize'] / 2)
+        else:   
+            if self.param['do_miss_check'] is True:
+                m[m] &= (np.abs(X_local[m,0]) < self.param['xsize'] / 2)
+                m[m] &= (np.abs(X_local[m,1]) < self.param['ysize'] / 2)
+        
         return X, rays
+    
+    def aperture(self, X, rays, m):
+        '''
+        Masks all rays that do pass through the aperature of the optic.
+        '''
+        self.aperture_check(X, rays,m)
+        return
+    
+    def aperture_check(self,X,rays,m):
+        '''
+        Generic optic does not mask currenlty
+        '''
+        pass
     
     def generate_normals(self, X, rays):
         m = rays['mask']
