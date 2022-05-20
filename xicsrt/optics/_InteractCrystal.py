Added an (optional) angular cutoff for the mosaic calculation. Some other minor fixes.

xicsrt/optics/_InteractCrystal.py
@@ -93,17 +93,14 @@ class InteractCrystal(InteractMirror):
         rays['mask'] = mask
         return rays
 
-    def angle_check(self, rays, norm, mask=None):
+    def angle_calc(self, rays , norm, mask=None):
         if mask is None:
             mask = rays['mask']
-
-        if self.param['check_bragg'] is False:
-            return mask
 
         D = rays['direction']
         W = rays['wavelength']
         m = mask
-        
+
         bragg_angle = np.zeros(m.shape, dtype=np.float64)
         dot = np.zeros(m.shape, dtype=np.float64)
         incident_angle = np.zeros(m.shape, dtype=np.float64)
@@ -111,8 +108,21 @@ class InteractCrystal(InteractMirror):
         # returns vectors that satisfy the bragg condition
         # only perform check on rays that have intersected the optic
         bragg_angle[m] = np.arcsin(W[m] / (2 * self.param['crystal_spacing']))
-        dot[m] = np.abs(np.einsum('ij,ij->i',D[m], -1 * norm[m]))
+        dot[m] = np.abs(np.einsum('ij,ij->i', D[m], -1 * norm[m], optimize=True))
         incident_angle[m] = (np.pi / 2) - np.arccos(dot[m] / xm.magnitude(D[m]))
+
+        return bragg_angle, incident_angle
+
+    def angle_check(self, rays, norm, mask=None):
+        if mask is None:
+            mask = rays['mask']
+
+        if self.param['check_bragg'] is False:
+            return mask
+
+        m = mask
+
+        bragg_angle, incident_angle = self.angle_calc(rays, norm, mask)
 
         #check which rays satisfy bragg, update mask to remove those that don't
         m[m] &= self.rocking_curve_filter(incident_angle[m], bragg_angle[m])

xicsrt/optics/_InteractMosaicCrystal.py
@@ -36,10 +36,18 @@ class InteractMosaicCrystal(InteractCrystal):
         mosaic_depth : int (15)
           The number of crystalite layers to model. This value will depend
           on the crystal structure and the incident x-ray energy.
+
+        mosaic_cutoff: float (None)
+          A numerical probability cutoff used to avoid calculation of the
+          mosaic reflection for angles far away from the nominal Bragg angle.
+          A value of 1e-8 would provide a 6-sigma cutoff in angles, while a
+          value of 1e-14 would provide an 8-sigma cutoff.
+          By default no cutoff is used.
         """
         config = super().default_config()
         config['mosaic_spread'] = 0.0
         config['mosaic_depth'] = 15
+        config['mosaic_cutoff'] = None
         return config
 
     def interact(self, rays, xloc, norm, mask=None):
@@ -51,18 +59,50 @@ class InteractMosaicCrystal(InteractCrystal):
         the HOPG 'focusing' qualities as well as the expected throughput.
         """
         if mask is None: mask = rays['mask']
+        m = mask
 
-        mosaic_mask = np.zeros(rays['mask'].shape, dtype=np.bool)
+        # If a cutoff is given then check for rays whose angle away from the
+        # nominal bragg angle falls outside of the cutoff and mark them as
+        # not-reflected.
+        if self.param['mosaic_cutoff'] is not None:
+            bragg_angle, incident_angle = self.angle_calc(rays, norm, mask)
+            angle_fwhm = self.param['mosaic_spread']
+            angle_sigma = angle_fwhm/(2 * np.sqrt(2 * np.log(2)))
+            angle_cutoff = np.sqrt(-1*np.log(self.param['mosaic_cutoff'])*2*angle_sigma**2)
+            m[m] = np.abs(bragg_angle[m] - incident_angle[m]) < angle_cutoff
 
-        for ii in range(self.param['mosaic_depth']):
-            temp_mask = (~ mosaic_mask) & mask
-            self.log.debug('  Mosaic iteration: {} rays: {}'.format(ii, sum(temp_mask)))
-            norm_mosaic = self.mosaic_normals(norm, temp_mask)
-            temp_mask = self.angle_check(rays, norm_mosaic, temp_mask)
-            rays = self.reflect_vectors(rays, xloc, norm_mosaic, temp_mask)
-            mosaic_mask[temp_mask] = True
-        mask &= mosaic_mask
-        rays['mask'] = mask
+            num_outside = np.sum(np.abs(bragg_angle[m] - incident_angle[m]) > angle_cutoff)
+            self.log.debug(f'mosaic angle_cutoff: {np.degrees(angle_cutoff):0.2f} deg, removing {num_outside} rays.')
+
+        # Check if any rays were within the cutoff.
+        if np.sum(m) > 0:
+            mosaic_mask = np.zeros(rays['mask'].shape, dtype=np.bool)
+
+            # Check for mosaic reflections up to the given 'depth'.
+            profiler.start('mosaic: loop')
+            for ii in range(self.param['mosaic_depth']):
+                temp_mask = (~ mosaic_mask) & mask
+                num_temp = np.sum(temp_mask)
+                if num_temp == 0:
+                    break
+
+                self.log.debug('  Mosaic iteration: {} rays: {}'.format(ii, num_temp))
+
+                profiler.start('mosaic: mosaic_normals')
+                norm_mosaic = self.mosaic_normals(norm, temp_mask)
+                profiler.stop('mosaic: mosaic_normals')
+
+                profiler.start('mosaic: angle_check')
+                temp_mask = self.angle_check(rays, norm_mosaic, temp_mask)
+                profiler.stop('mosaic: angle_check')
+
+                profiler.start('mosaic: reflect_vectors')
+                rays = self.reflect_vectors(rays, xloc, norm_mosaic, temp_mask)
+                profiler.stop('mosaic: reflect_vectors')
+
+                mosaic_mask[temp_mask] = True
+            profiler.stop('mosaic: loop')
+            mask &= mosaic_mask
 
         return rays
 

xicsrt/tools/xicsrt_spread.py
@@ -317,7 +317,7 @@ def vector_dist_flat_gaussian(spread, num_samples):
     theta = _parse_spread_single(spread)
 
     # Convert the angluar hwhm to sigma.
-    sigma = theta / (np.sqrt(2 * np.log(2)))
+    sigma = theta[0] / (np.sqrt(2 * np.log(2)))
 
     # Calculate the equivilant width in x & y on a plane with z=1.
     xsigma = np.sin(sigma)