ae0999de8d675a12c5ebdae1389db1ac800ab8e1
Novimir Antoniuk Pablant <npablant@pppl.gov>
2023-03-08 09:37:47 -0500

    Incremented version from 0.8.6 to 0.8.7.

--- xicsrt/_version.py
+++ xicsrt/_version.py
@@ -14,7 +14,7 @@ revision:
   - New release that does not break any compatibility.
 """
 
-__version__="0.8.6"
+__version__="0.8.7"
 
 from packaging import version
 from xicsrt.util import mirlogging
