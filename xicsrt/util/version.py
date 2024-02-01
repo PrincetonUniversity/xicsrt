bebc3686f2bc4e6f8f4369ff83b33e8faf30e417
Novimir Antoniuk Pablant <npablant@pppl.gov>
2024-02-01 09:19:17 -0500

    Update version check logic.

--- xicsrt/util/version.py
+++ xicsrt/util/version.py
@@ -16,7 +16,7 @@ def warn_version(v_string):
     v_input = version.parse(v_string)
     v_current = version.parse(_version.__version__)
 
-    if (v_input.major < v_current.major) or (v_input.minor < v_current.minor):
+    if (v_input.major < v_current.major) or ((v_input.major == v_current.major) and (v_input.minor < v_current.minor)):
         log.warning('This config is for an older version of xicsrt. Some options may have changed.')
-    elif (v_input.major > v_current.major) or (v_input.minor > v_current.minor):
+    elif (v_input.major > v_current.major) or ((v_input.major == v_current.major) and (v_input.minor > v_current.minor)):
         log.warning('This config is for a newer version of xicsrt. Please upgrade xicsrt (pip install --upgrade xicsrt).')
